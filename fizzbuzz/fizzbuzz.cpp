d53443eb84e1f40116c28d4c10a742734cb090f9
mxkay <kayalyxvail@gmail.com>
2020-07-29 14:36:55 -0400

    Reformat fizzbuzz c++ solution

--- fizzbuzz/fizzbuzz.cpp
+++ fizzbuzz/fizzbuzz.cpp
@@ -4,16 +4,12 @@ using namespace std;
 
 #define MAX_NUMBER 100
 
-void fizzbuzz(int max_number) {
-    for ( int i = 1; i <= max_number; i++ ) {
+int main(void) {
+    for ( int i = 1; i <= MAX_NUMBER; i++ ) {
         if (!(i%15)) cout << "FizzBuzz";
         else if (!(i%3)) cout << "Fizz";
         else if (!(i%5)) cout << "Buzz";
         else cout << i;
         cout << endl;
     }
-}
-
-int main(void) {
-    fizzbuzz(MAX_NUMBER);
 }
