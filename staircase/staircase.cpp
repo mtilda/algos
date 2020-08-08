fbd25e21aceec8f9df6534c01dd893d65d6abb96
mxkay <kayalyxvail@gmail.com>
2020-08-07 22:50:31 -0400

    Add comments

--- staircase/staircase.cpp
+++ staircase/staircase.cpp
@@ -5,9 +5,17 @@ using namespace std;
 #define STAIRCASE_LENGTH 10
 
 int main() {
+
+    // DEFINE string to represent a single step in the staircase
     string str (STAIRCASE_LENGTH, 32);
+
+    // FOR EACH step of the staircase
     for( int i = STAIRCASE_LENGTH - 1; i >= 0; i-- ) {
+
+        // REPLACE the final space in the string with a #
         str[i] = '#';
+
+        // PRINT the current step
         cout << str << endl;
     }
 }
